02b640cf95747ef835d3b143814c0d7770336c65
elyseeelius <elyseeelius@outlook.com>
2019-08-10 20:29:10 -0400

    new version

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById('menu');
+const nav = document.getElementsByTagName('nav')[0]
+const svgWrapper = document.querySelector('.svgWrapper')
+menu.addEventListener('click', function(){
+    nav.style.display = 'block';
+    menu.style.display = 'none'
+    svgWrapper.style.marginTop= '20%';
+})
