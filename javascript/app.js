b73ad570fd8ebd70cc2f6541d6a7782defa508eb
elyseeelius <elyseeelius@outlook.com>
2019-08-11 20:03:24 -0400

    typing effect

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,12 @@
-const menu = document.getElementById('menu');
-const nav = document.getElementsByTagName('nav')[0]
-const svgWrapper = document.querySelector('.svgWrapper')
-menu.addEventListener('click', function(){
-    nav.style.display = 'block';
-    menu.style.display = 'none'
-    svgWrapper.style.marginTop= '20%';
-})
+// const menu = document.getElementById('menu');
+// const nav = document.getElementsByTagName('nav')[0]
+// const svgWrapper = document.querySelector('.svgWrapper')
+// menu.addEventListener('click', function(){
+//     nav.style.display = 'block';
+//     menu.style.display = 'none'
+//     svgWrapper.style.marginTop= '20%';
+// })
+
+
+const skills = document.querySelector('.skills');
+console.log(skills.textContent.length)
